b1468fb94dd72ec742315b918452cdea05e3f997
Pete Forrest <pete@itzalive.co.uk>
2019-09-12 15:27:07 +1200

    Read graph from DOT file

--- Gigaclear_TechTest/Gigaclear_TechTest.Test/GraphTests.cs
+++ Gigaclear_TechTest/Gigaclear_TechTest.Test/GraphTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.IO;
 
 namespace Gigaclear_TechTest.Test
 {
@@ -159,6 +160,111 @@ namespace Gigaclear_TechTest.Test
 
             // Act / Assert
             Assert.Throws<ArgumentException>(() => graph.AddEdge(edge));
+        }
+
+        [Test]
+        public void ReadFromDotFileSingleNode()
+        {
+            // Arrange
+            var lines = "A [type=Cabinet];";
+
+            // Act
+            var graph = readDotFileHelper(lines);
+
+            // Assert
+            Assert.That(graph.Nodes.Count, Is.EqualTo(1));
+            Assert.That(graph.Edges.Count, Is.EqualTo(0));
+            Assert.That(graph.Nodes[0].Id, Is.EqualTo("A"));
+            Assert.That(graph.Nodes[0].Type, Is.EqualTo(NodeType.Cabinet));
+        }
+
+        [Test]
+        public void ReadFromDotFileSingleNodeAlternate()
+        {
+            // Arrange
+            var lines = "C [type=Pot];";
+
+            // Act
+            var graph = readDotFileHelper(lines);
+
+            // Assert
+            Assert.That(graph.Nodes.Count, Is.EqualTo(1));
+            Assert.That(graph.Edges.Count, Is.EqualTo(0));
+            Assert.That(graph.Nodes[0].Id, Is.EqualTo("C"));
+            Assert.That(graph.Nodes[0].Type, Is.EqualTo(NodeType.Pot));
+        }
+
+        [Test]
+        public void ReadFromDotFileMultipleNodes()
+        {
+            // Arrange
+            var lines = "C [type=Pot];\r\n" +
+                        "ASD [type=Chamber];";
+
+            // Act
+            var graph = readDotFileHelper(lines);
+
+            // Assert
+            Assert.That(graph.Nodes.Count, Is.EqualTo(2));
+            Assert.That(graph.Edges.Count, Is.EqualTo(0));
+            Assert.That(graph.Nodes[0].Id, Is.EqualTo("C"));
+            Assert.That(graph.Nodes[0].Type, Is.EqualTo(NodeType.Pot));
+            Assert.That(graph.Nodes[1].Id, Is.EqualTo("ASD"));
+            Assert.That(graph.Nodes[1].Type, Is.EqualTo(NodeType.Chamber));
+        }
+
+        [Test]
+        public void ReadFromDotFileSingleEdge()
+        {
+            // Arrange
+            var lines = "START [type=Pot];\r\n" +
+                        "End [type=Chamber];\r\n" +
+                        "START -- End  [material=verge, length=100];";
+
+            // Act
+            var graph = readDotFileHelper(lines);
+
+            // Assert
+            Assert.That(graph.Nodes.Count, Is.EqualTo(2));
+            Assert.That(graph.Edges.Count, Is.EqualTo(1));
+            Assert.That(graph.Edges[0].StartNode.Id, Is.EqualTo("START"));
+            Assert.That(graph.Edges[0].EndNode.Id, Is.EqualTo("End"));
+            Assert.That(graph.Edges[0].Type, Is.EqualTo(EdgeType.Verge));
+            Assert.That(graph.Edges[0].Length, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ReadFromDotFileMultipleEdge()
+        {
+            // Arrange
+            var lines = "START [type=Pot];\r\n" +
+                        "End [type=Chamber];\r\n" +
+                        "Node [type=Cabinet];\r\n" +
+                        "START -- Node  [material=verge, length=100];\r\n" +
+                        "End -- Node  [material=road, length=56];";
+
+            // Act
+            var graph = readDotFileHelper(lines);
+
+            // Assert
+            Assert.That(graph.Nodes.Count, Is.EqualTo(3));
+            Assert.That(graph.Edges.Count, Is.EqualTo(2));
+            Assert.That(graph.Edges[0].StartNode.Id, Is.EqualTo("START"));
+            Assert.That(graph.Edges[0].EndNode.Id, Is.EqualTo("Node"));
+            Assert.That(graph.Edges[0].Type, Is.EqualTo(EdgeType.Verge));
+            Assert.That(graph.Edges[0].Length, Is.EqualTo(100));
+            Assert.That(graph.Edges[1].StartNode.Id, Is.EqualTo("End"));
+            Assert.That(graph.Edges[1].EndNode.Id, Is.EqualTo("Node"));
+            Assert.That(graph.Edges[1].Type, Is.EqualTo(EdgeType.Road));
+            Assert.That(graph.Edges[1].Length, Is.EqualTo(56));
+        }
+
+        private Graph readDotFileHelper(string dotFileLines)
+        {
+            string contents = $"strict graph \"\" {{\r\n{dotFileLines}\r\n}}\r\n";
+            string filename = Path.GetTempFileName();
+            File.WriteAllText(filename, contents);
+            return Graph.ReadFromDotFile(filename);
         }
     }
 }
--- Gigaclear_TechTest/Gigaclear_TechTest/Graph.cs
+++ Gigaclear_TechTest/Gigaclear_TechTest/Graph.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Gigaclear_TechTest
 {
     public class Graph
     {
+        private const string nodeIdRegex = "[a-zA-Z0-9]+";
+        private const string graphDotFileRegex = @"strict graph ""(.*)"" \{([\s\S]+)\}";
+        private const string nodeDotFileRegex = @"(" + nodeIdRegex + @") \[([^\]]+)\]";
+        private const string edgeDotFileRegex = @"(" + nodeIdRegex + @") -- (" + nodeIdRegex + @") +\[([^\]]+)\]";
+
         public List<Node> Nodes { get; } = new List<Node>();
 
         public List<Edge> Edges { get; } = new List<Edge>();
 
         public void AddNode(Node node)
         {
-            if (!Nodes.Contains(node))
-                Nodes.Add(node);
+            if (Nodes.Contains(node))
+                return;
+
+            if (Nodes.Any((a) => a.Id == node.Id))
+                throw new ArgumentException("Node with same Id already exists");
+
+            Nodes.Add(node);
         }
 
         public void AddEdge(Edge edge)
@@ -24,5 +38,63 @@ namespace Gigaclear_TechTest
             if (!Edges.Contains(edge))
                 Edges.Add(edge);
         }
+
+        public Node GetNodeById(string id)
+        {
+            return Nodes.Single((a) => a.Id == id);
+        }
+
+        public static Graph ReadFromDotFile(string filename)
+        {
+            Graph graph = new Graph();
+
+            var fileContents = File.ReadAllText(filename);
+
+            if (!Regex.IsMatch(fileContents, graphDotFileRegex))
+                throw new FormatException($"File '{filename}' is not in correct format");
+
+            var graphMatch = Regex.Match(fileContents, graphDotFileRegex);
+            var lines = graphMatch.Groups[2].Value.Split(';');
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                graph.processDotFileLine(line.Trim());
+            }
+
+            return graph;
+        }
+
+        private void processDotFileLine(string line)
+        {
+
+            if (Regex.IsMatch(line, nodeDotFileRegex))
+            {
+                var nodeMatch = Regex.Match(line, nodeDotFileRegex);
+                var arguments = readArgumentsList(nodeMatch.Groups[2].Value);
+                var node = new Node(nodeMatch.Groups[1].Value, (NodeType)Enum.Parse(typeof(NodeType), arguments["type"]));
+                AddNode(node);
+            }
+            else if (Regex.IsMatch(line, edgeDotFileRegex))
+            {
+                var edgeMatch = Regex.Match(line, edgeDotFileRegex);
+                var startNode = GetNodeById(edgeMatch.Groups[1].Value);
+                var endNode = GetNodeById(edgeMatch.Groups[2].Value);
+                var arguments = readArgumentsList(edgeMatch.Groups[3].Value);
+                var edge = new Edge(startNode, endNode, (EdgeType)Enum.Parse(typeof(EdgeType), arguments["material"], true), int.Parse(arguments["length"]));
+                AddEdge(edge);
+            }
+            else
+            {
+                throw new Exception("Unknown line in DOT graph");
+            }
+        }
+
+        private static IDictionary<string, string> readArgumentsList(string argumentsList)
+        {
+            return Regex.Matches(argumentsList, "([^?=, ]+)(=([^,]*))?").Cast<Match>().ToDictionary(x => x.Groups[1].Value, x => x.Groups[3].Value);
+        }
+
     }
 }
